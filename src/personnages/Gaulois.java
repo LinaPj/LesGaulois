ec6c17c76be4b3635ec5998d42688d1f14606837
Lina Lily <linalily@Linas-MacBook-Pro.local>
2025-11-02 13:53:51 +0100

    tp 2 final

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -2,91 +2,80 @@ package personnages;
 
 import objets.Village;
 
-public class Gaulois 
-{
-	private String nom;
-	private int force;
-	private int effetPotion=1;
-	private Village village;
+public class Gaulois {
+    private String nom;
+    private int force;
+    private int effetPotion = 1;
+    private Village village;
 
-	
-	
-	
-	public Gaulois(String nom, int force){
-		this.nom = nom;
-		this.force = force;
-	}
+    // Constructeur
+    public Gaulois(String nom, int force) {
+        this.nom = nom;
+        this.force = force;
+    }
 
+    public String getNom() {
+        return nom;
+    }
 
-	public String getNom() {
-		return nom;
-	}
-	
-	public void parler(String texte) {
-		System.out.println(prendreParole() + "\"" + texte + "\"");
-	}
-	
-	
+    public void frapper(Romain romain) {
+        System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+        int forceCoup = (force * effetPotion) / 3;
+        romain.recevoirCoup(forceCoup);
+        if (effetPotion > 1) {
+            effetPotion--;
+        }
+    }
 
+    
+    public void boirePotion(int forcePotion) {
+        this.effetPotion = forcePotion;
+    }
 
-	
-		
-	private String prendreParole() {
-		
-		return "Le gaulois " + nom + " : ";
-	}
- 
+    
+    public void parler(String texte) {
+        System.out.println(prendreParole() + "\"" + texte + "\"");
+    }
 
+    private String prendreParole() {
+        return "Le gaulois " + nom + " : ";
+    }
 
+   
+    public String toString() {
+        return nom;
+    }
 
+    
+    public void setVillage(Village village) {
+        this.village = village;
+    }
 
+    public Village getVillage() {
+        return village;
+    }
 
-	
+    
+    public void sePresenter() {
+        if (village == null) {
+            System.out.println("Le Gaulois " + nom + " : \"Bonjour, je m'appelle " + nom +
+                    ". Je voyage de villages en villages.\"");
+        } else if (village.getChef() == this) {
+            System.out.println("Le Gaulois " + nom + " : \"Bonjour, je m'appelle " + nom +
+                    ". Je suis le chef du village " + village.getNom() + ".\"");
+        } else {
+            System.out.println("Le Gaulois " + nom + " : \"Bonjour, je m'appelle " + nom +
+                    ". J'habite le village " + village.getNom() + ".\"");
+        }
+    }
 
-	public String toString() 
-	{
-		return nom;
-	}
-	
-	
-	public static void main(String[] args) 
-	{
-		Gaulois asterix = new Gaulois("Astérix", 8);
+    
+    public static void main(String[] args) {
+        Gaulois asterix = new Gaulois("Astérix", 8);
+        System.out.println(asterix);
+    }
+}
 
-        
-        System.out.print(asterix);
-		
-	}
-	
-	
-	public void frapper(Romain romain) 
-	{
-		 System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-		    int forceCoup = (force * effetPotion) / 3;
-		    romain.recevoirCoup(forceCoup);
-		    if (effetPotion > 1) {
-		        effetPotion--;
-		    }
-		
-		
-	}
-	
-	public void boirePotion(int forcePotion) 
-	{
-	    this.effetPotion = forcePotion;
-	}
-
-
-	
-
-
-	public void setVillage(Village village) {
-		this.village = village;
-	}
-
-	
-	
-	
 	
 
 	
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,50 +1,92 @@
 package personnages;
 
-public class Romain
-{
-	private String nom;
-	private int force;
-	
-	
-	public Romain(String nom, int force) {
-		this.nom = nom;
-		this.force = force;
-	}
+import objets.Equipement;
 
+public class Romain {
+    private String nom;
+    private int force;
+    private Equipement[] equipements = new Equipement[2];
+    private int nbEquipement = 0;
 
-	public String getNom() {
-		return nom;
-	}
+    // Constructeur
+    public Romain(String nom, int force) {
+        this.nom = nom;
+        this.force = force;
+        assert isInvariantVerified() : "Force négative à la création !";
+    }
 
+    // Getters
+    public String getNom() {
+        return nom;
+    }
 
-	public void parler(String texte) 
-	{
-		System.out.println(prendreParole() + "\"" + texte + "\"");
-		}
+    // Méthode parler
+    public void parler(String texte) {
+        System.out.println(prendreParole() + "\"" + texte + "\"");
+    }
 
+    private String prendreParole() {
+        return "Le romain " + nom + " : ";
+    }
 
-	private String prendreParole() 
-	{
-		return "Le romain " + nom + " : ";
-		
-	}
-	
-	
-	public void recevoirCoup(int forceCoup) 
-	{
-		force -= forceCoup;
-	    if (force > 0) 
-	    {
-	    	System.out.println("Le Romain " + nom + " a encore " + force + " de force.");
-	     } else 
-	     {
-	     
-	    	 System.out.println("Le Romain " + nom + " est à terre !");
-	        }
-		
-	} 
-	
+    // Recevoir un coup
+    public void recevoirCoup(int forceCoup) {
+        // Précondition
+        assert forceCoup > 0 : "Force du coup non positive !";
 
-	
+        int ancienneForce = force;
+        force -= forceCoup;
 
+        if (force <= 0) {
+            force = 0;
+            parler("J'abandonne !");
+        } else {
+            parler("Aïe");
+        }
+
+        // Postcondition
+        assert force < ancienneForce : "La force n'a pas diminué !";
+        assert isInvariantVerified() : "Invariant non respecté (force < 0)";
+    }
+
+    // Vérification de l’invariant
+    private boolean isInvariantVerified() {
+        return force >= 0;
+    }
+
+    
+    public void sEquiper(Equipement equipement) {
+        switch (nbEquipement) {
+            case 2:
+                System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+                break;
+            case 1:
+                if (equipements[0] == equipement) {
+                    System.out.println("Le soldat " + nom + " possède déjà un " + equipement + " !");
+                } else {
+                    equipements[nbEquipement] = equipement;
+                    nbEquipement++;
+                    System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement + ".");
+                }
+                break;
+            case 0:
+                equipements[nbEquipement] = equipement;
+                nbEquipement++;
+                System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement + ".");
+                break;
+        }
+    }
+
+    // Pour test
+    public static void main(String[] args) {
+        Romain minus = new Romain("Minus", 6);
+
+       
+
+        minus.sEquiper(Equipement.CASQUE);
+        minus.sEquiper(Equipement.CASQUE);
+        minus.sEquiper(Equipement.BOUCLIER);
+        minus.sEquiper(Equipement.CASQUE);
+    }
 }
+
